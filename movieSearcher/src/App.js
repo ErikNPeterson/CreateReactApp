3a33333f81e58ffe98e6f5b8384072cae703fbf9
ErikNPeterson <erikeh1@gmail.com>
2018-10-19 17:46:16 -0400

    Movie App now working with Search URL mods

--- movieSearcher/src/App.js
+++ movieSearcher/src/App.js
@@ -23,12 +23,12 @@ class App extends Component {
 
     // this.state ={rows: movieRows}
 
-    this.performSearch("ant man");
+    // this.performSearch("ant man");
   }
 
   performSearch(searchTerm){
     console.log("Perform search using moviedb")
-    const urlString = "https://api.themoviedb.org/3/search?api_key=228d2a3b60dddb8c258df6928f086459&query=" + searchTerm ;
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=228d2a3b60dddb8c258df6928f086459&query=" + searchTerm ;
     $.ajax({
       url: urlString,
       success:(searchResults) => {
@@ -40,8 +40,8 @@ class App extends Component {
         var movieRows = []; 
 
         results.forEach((movie) => {
-          movie.poster_src = "";
-          console.log(movie.title)
+          movie.poster_src = "https://image.tmdb.org/t/p/w185/" + movie.poster_path;
+          console.log(movie.poster_path);
           const movieRow = <MovieRow key={movie.id} movie={movie}/>
           movieRows.push(movieRow)
         })
@@ -56,7 +56,7 @@ class App extends Component {
   }
 
   searchChangeHandler(event){
-    console.log(event.target.value)
+    // console.log(event.target.value)
     const boundObject = this
     const searchTerm = event.target.value
     boundObject.performSearch(searchTerm);
